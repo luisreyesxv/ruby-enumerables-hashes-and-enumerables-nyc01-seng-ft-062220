83107ef41b6f901d507b305e1e332a3df8c4e4e8
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-14 22:28:50 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,5 +9,5 @@
 def select_winner(passengers)
   # add the code snippet here!
   winner =""
-  passengers.each {|suite,name| if suite== :suite_a && name.start_with?("A") winner = name}
+  passengers.each {|suite,name| if suite== :suite_a && name.start_with?("A") winner = name end}
 end
